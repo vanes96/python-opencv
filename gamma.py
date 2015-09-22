9bc667e7341175f914fbaa6a1cd1401a39ff20d4
doomstal <keyboardsurfer@mail.ru>
2015-09-22 15:13:43 +0300

    update

--- gamma.py
+++ gamma.py
@@ -6,9 +6,11 @@ import numpy as np
 img_orig = cv2.imread("Lenna.png")
 img_orig = np.double(img_orig) / 255.0
 
-gamma = float(raw_input("gamma:") or 1.0)
+mul = float(raw_input("multiplier (default 1.0) :") or 1.0)
+gamma = float(raw_input("gamma (default 1.0):") or 1.0)
 
-img_res = cv2.pow(img_orig, 1.0 / gamma)
+img_res = cv2.pow(img_orig, gamma)
+img_res = cv2.scaleAdd(img_res, mul - 1.0, img_res)
 
 cv2.imshow("original", img_orig)
 cv2.moveWindow("original", 0, 0)
